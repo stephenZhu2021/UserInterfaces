1cd80c615a76760a857ce73ac27289fb40a435df
Stephanie Moreau <stephanie.moreau@gmail.com>
2022-02-02 18:12:06 -0500

    form js part 1

--- scripts/forms.js
+++ scripts/forms.js
@@ -1,4 +1,4 @@
-
+let isFormValid = true; // keeps track of invalid form data
 
 
 
@@ -14,3 +14,48 @@ function validateEmail(stringToCheck){
 	return emailRegExp.test(stringToCheck);
 
 }
+
+function validateForm(event){
+	//prevent form from submitting
+	event.preventDefault();
+
+	// validate name - required
+	if ( validateRequired("txtName") ){
+		showError("txtName", "Name is required");
+	} else {
+		showSuccess("txtName");
+	}
+
+	// validate email - required + email
+
+	// validate password - required + 6 to 12 characters
+
+	// validate passwor confirm - required + matches password
+
+	// validate city - required
+
+	//validate checkbox - required
+
+	// if all if valid then submit form
+}
+
+function validateRequired (elemID){
+	return document.getElementById( elemID ).value == "";
+}
+
+function showError(elemID, message){
+	isFormValid = false;
+
+	// show error message in small element
+	document.querySelector("#" + elemID + " ~ small").innerText = message;
+
+	// TODO - add error class to small, and elemID
+}
+
+function showSuccess(elemID){
+	// TO DO - remove error class to small, and elemID
+	// TO DO - remove small innerText
+}
+
+/* event listener */
+document.getElementById("formJS").addEventListener("submit", validateForm );
